aaf445c23aab845ba0d2671a102f13bc5d2eb39b
boomNDS <boomsixboom@gmail.com>
2024-06-19 00:04:24 +0700

    build: add swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Main');
+
+  const configService = app.get(ConfigService);
+
+  // Swagger configuration
+  const options = new DocumentBuilder()
+    .setTitle('Board backend')
+    .setDescription('Board backend API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  // Logging the application's URL
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
+  logger.log(`🚀 Application is running on: http://localhost:${port}/api`);
 }
+
 bootstrap();
