81d44ce03871a1c24bac23a989121b5270ade1f0
boomNDS <boomsixboom@gmail.com>
2024-06-20 02:23:44 +0700

    refactor: remove unused

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,9 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './service/user.service';
 import { PrismaService } from '../prisma/service/prisma.service';
-import { UserController } from './controller/user.controller';
 
 @Module({
   providers: [UserService, PrismaService],
-  controllers: [UserController],
   exports: [UserService],
 })
 export class UserModule {}
